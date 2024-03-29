ab9fff079716acfca55c09e4ed5fce74f3305669
mohibUllah07 <122685562+mohibUllah07@users.noreply.github.com>
2024-03-29 16:32:37 +0500

    Add files via upload

---
+++ delegates in c sharp/delegates in c sharp/Program.cs
@@ -0,0 +1,35 @@
+﻿
+
+
+maths.calculaor calculations;
+
+
+calculations = maths.add;
+Console.WriteLine(calculations(45,5));
+
+
+class maths
+{
+    public delegate double calculaor (double a , double b);
+    public static double add(double a, double b){
+        return a + b;
+    }
+
+
+    public static double minus(double a, double b)
+    {
+        return a - b;
+    }
+
+
+    public static double multiply(double a, double b)
+    {
+        return a * b;
+    }
+
+    public static double devided(double a, double b)
+    {
+        return a / b;
+    }
+}
+
